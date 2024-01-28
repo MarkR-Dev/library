862307ec19b47fd56bc4a612d3bd3e0751d556d2
Mark Rodgers <markrodgers.dev@gmail.com>
2024-01-28 16:33:08 +0000

    Add function to display all books

--- script.js
+++ script.js
@@ -1,13 +1,56 @@
-const myLibrary = [];
+"use strict"
 
-function Book(title, author, pages, hasRead){
+const bookContainer = document.querySelector("#book-container");
+
+// remove these later
+const myLibrary = [
+    new Book("The Hobbit", "author", 123, true),
+    new Book("Batman", "author", 234, false),
+    new Book("Joker", "author", 443, true)
+];
+
+function Book(title, author, pages, hasRead) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.hasRead = hasRead;
 }
 
-function addBookToLibrary(){
-    // do stuff
+function addBookToLibrary() {
 }
+
+function displayLibrary() {
+    myLibrary.forEach(book => {
+
+        const bookCard = document.createElement("div");
+        const bookTitle = document.createElement("p");
+        const bookAuthor = document.createElement("p");
+        const bookPages = document.createElement("p");
+        const bookHasRead = document.createElement("p");
+        
+        bookCard.classList.add("book-card");
+
+        bookTitle.textContent = book.title;
+        bookTitle.classList.add("book-title");
+
+        bookAuthor.textContent = book.author;
+        bookAuthor.classList.add("book-author");
+
+        bookPages.textContent = book.pages;
+        bookPages.classList.add("book-pages");
+
+        bookHasRead.textContent = book.hasRead;
+        bookHasRead.classList.add("book-has-read");
+
+        bookCard.appendChild(bookTitle);
+        bookCard.appendChild(bookAuthor);
+        bookCard.appendChild(bookPages);
+        bookCard.appendChild(bookHasRead);
+
+        bookContainer.appendChild(bookCard);
+    });
+}
+
+// console.log(myLibrary)
+displayLibrary()
 
